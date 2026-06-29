e14c3d767a31a40dbe733e347c3416e6fa9fac78
IwantgrandpademonOnIos <edevsi28@gmail.com>
2026-06-29 17:31:21 +1000

    Update IVBackgroundSprite.hpp

--- src/class/IVBackgroundSprite.hpp
+++ src/class/IVBackgroundSprite.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <IVEvent.hpp>
+#include "IVEvent.hpp"
 
 GEODE_NS_IV_BEGIN
 
@@ -18,34 +18,45 @@ public:
     );
     static BackgroundSprite* create();
     bool init() override;
+
 public:
     void addBackgroundNode(cocos2d::CCNode* node);
     void addOutlineNode(cocos2d::CCNode* node);
     void addTextNode(cocos2d::CCNode* node);
+
     void removeBackgroundNode(cocos2d::CCNode* node);
     void removeOutlineNode(cocos2d::CCNode* node);
     void removeTextNode(cocos2d::CCNode* node);
+
     void setBackgroundColor(cocos2d::ccColor4B const& color);
     void setOutlineColor(cocos2d::ccColor4B const& color);
     void setTextColor(cocos2d::ccColor4B const& color);
+
 public:
     void setContentSize(cocos2d::CCSize const& size) override;
     void visit() override;
+
 protected:
     void updateBackgroundColorNodes();
     void updateOutlineColorNodes();
     void updateTextColorNodes();
+
 protected:
     cocos2d::extension::CCScale9Sprite* m_fill = nullptr;
     cocos2d::extension::CCScale9Sprite* m_outline = nullptr;
+
     bool m_shouldUpdateLayout = false;
+
     std::reference_wrapper<cocos2d::ccColor4B const> m_backgroundColor;
     std::reference_wrapper<cocos2d::ccColor4B const> m_outlineColor;
     std::reference_wrapper<cocos2d::ccColor4B const> m_textColor;
+
     geode::cocos::CCArrayExt<cocos2d::CCNode*> m_backgroundColorNodes;
     geode::cocos::CCArrayExt<cocos2d::CCNode*> m_outlineColorNodes;
     geode::cocos::CCArrayExt<cocos2d::CCNode*> m_textColorNodes;
-    geode::EventListener<IVSettingFilter> m_colorListener;
+
+    // Geode 5 removed EventListener entirely, so this member is gone.
+    // geode::EventListener<IVSettingFilter> m_colorListener;
 };
 
 GEODE_NS_IV_END
