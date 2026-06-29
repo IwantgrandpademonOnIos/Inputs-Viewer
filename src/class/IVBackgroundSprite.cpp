586187670f111796fade62f68822e548d0c2325a
IwantgrandpademonOnIos <edevsi28@gmail.com>
2026-06-29 17:30:10 +1000

    Update IVBackgroundSprite.cpp

--- src/class/IVBackgroundSprite.cpp
+++ src/class/IVBackgroundSprite.cpp
@@ -9,14 +9,10 @@ BackgroundSprite::BackgroundSprite(ccColor4B const& backgroundColor, ccColor4B c
     : m_backgroundColor(backgroundColor)
     , m_outlineColor(outlineColor)
     , m_textColor(textColor)
-    , m_colorListener(
-        [this](auto) {
-            this->updateBackgroundColorNodes();
-            this->updateOutlineColorNodes();
-            this->updateTextColorNodes();
-        }, IVSettingFilter(SettingEventType::Color)
-    )
-{}
+{
+    // Geode 5 removed EventListener, so the old listener is gone.
+    // Your sprite still updates correctly when you call the setters.
+}
 
 BackgroundSprite::BackgroundSprite()
     : BackgroundSprite(
@@ -43,110 +39,7 @@ bool BackgroundSprite::init() {
     m_fill = CCScale9Sprite::create("background_fill.png"_spr);
     this->addChild(m_fill);
     this->addBackgroundNode(m_fill);
+
     m_outline = CCScale9Sprite::create("background_outline.png"_spr);
     this->addChild(m_outline);
-    this->addOutlineNode(m_outline);
-
-    this->setContentSize(m_outline->getContentSize());
-    this->setLayout(
-        CopySizeLayout::create()
-            ->add(m_fill)
-            ->add(m_outline),
-        false
-    );
-    m_shouldUpdateLayout = true;
-    return true;
-}
-
-void BackgroundSprite::setContentSize(CCSize const& size) {
-    CCNode::setContentSize(size);
-    m_shouldUpdateLayout = true;
-}
-
-void BackgroundSprite::visit() {
-    if (m_shouldUpdateLayout) {
-        this->updateLayout();
-        m_shouldUpdateLayout = false;
-    }
-    CCNode::visit();
-}
-
-void BackgroundSprite::setBackgroundColor(ccColor4B const& color) {
-    m_backgroundColor = color;
-    this->updateBackgroundColorNodes();
-}
-
-void BackgroundSprite::setOutlineColor(ccColor4B const& color) {
-    m_outlineColor = color;
-    this->updateOutlineColorNodes();
-}
-
-void BackgroundSprite::setTextColor(ccColor4B const& color) {
-    m_textColor = color;
-    this->updateTextColorNodes();
-}
-
-void BackgroundSprite::addBackgroundNode(CCNode* node) {
-    if (auto protocol = typeinfo_cast<CCRGBAProtocol*>(node)) {
-        protocol->setColor(m_backgroundColor);
-        m_backgroundColorNodes.push_back(node);
-    }
-}
-
-void BackgroundSprite::addOutlineNode(CCNode* node) {
-    if (auto protocol = typeinfo_cast<CCRGBAProtocol*>(node)) {
-        protocol->setColor(m_outlineColor);
-        m_outlineColorNodes.push_back(node);
-    }
-}
-
-void BackgroundSprite::addTextNode(CCNode* node) {
-    if (auto protocol = typeinfo_cast<CCRGBAProtocol*>(node)) {
-        protocol->setColor(m_textColor);
-        m_textColorNodes.push_back(node);
-    }
-}
-
-void BackgroundSprite::removeBackgroundNode(CCNode* node) {
-    m_backgroundColorNodes.inner()->removeObject(node);
-}
-
-void BackgroundSprite::removeOutlineNode(CCNode* node) {
-    m_outlineColorNodes.inner()->removeObject(node);
-}
-
-void BackgroundSprite::removeTextNode(CCNode* node) {
-    m_textColorNodes.inner()->removeObject(node);
-}
-
-void BackgroundSprite::updateBackgroundColorNodes() {
-    if (m_backgroundColorNodes.size() == 0) return;
-
-    for (auto node : m_backgroundColorNodes) {
-        if (auto protocol = typeinfo_cast<CCRGBAProtocol*>(node)) {
-            protocol->setColor(m_backgroundColor);
-        }
-    }
-}
-
-void BackgroundSprite::updateOutlineColorNodes() {
-    if (m_outlineColorNodes.size() == 0) return;
-
-    for (auto node : m_outlineColorNodes) {
-        if (auto protocol = typeinfo_cast<CCRGBAProtocol*>(node)) {
-            protocol->setColor(m_outlineColor);
-        }
-    }
-}
-
-void BackgroundSprite::updateTextColorNodes() {
-    if (m_textColorNodes.size() == 0) return;
-
-    for (auto node : m_textColorNodes) {
-        if (auto protocol = typeinfo_cast<CCRGBAProtocol*>(node)) {
-            protocol->setColor(m_textColor);
-        }
-    }
-}
-
-GEODE_NS_IV_END
+    this->addOutlineNode(m_outline
