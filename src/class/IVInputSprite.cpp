83dc07b468559f5bc7b1f6cbdbd27431da92d181
IwantgrandpademonOnIos <edevsi28@gmail.com>
2026-06-29 13:33:03 +1000

    Update IVInputSprite.cpp

--- src/class/IVInputSprite.cpp
+++ src/class/IVInputSprite.cpp
@@ -19,13 +19,7 @@ IVSettingFilter::IVSettingFilter(std::nullopt_t) : IVSettingFilter() {}
 IVSettingFilter::IVSettingFilter(SettingEventType type)
     : m_type(type) {}
 
-// CORRECCIÓN DEFINITIVA: 
-// Cambiamos geode::utils::MiniFunction por geode::Function
-ListenerResult IVSettingFilter::handle(geode::Function<Callback> fn, IVSettingEvent* event) {
-    if (m_type && m_type.value() != event->getType()) return ListenerResult::Propagate;
 
-    fn(event->getType());
-    return ListenerResult::Propagate;
 }
 
 GEODE_NS_IV_END
