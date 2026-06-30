d9b46b26b31c7fa6558e9ed9b697d72f0c9aa631
IwantgrandpademonOnIos <edevsi28@gmail.com>
2026-06-30 10:22:58 +1000

    Update IVBackgroundSprite.cpp

--- src/class/IVBackgroundSprite.cpp
+++ src/class/IVBackgroundSprite.cpp
@@ -42,4 +42,4 @@ bool BackgroundSprite::init() {
 
     m_outline = CCScale9Sprite::create("background_outline.png"_spr);
     this->addChild(m_outline);
-    this->addOutlineNode(m_outline
+    this->addOutlineNode(m_outline);
