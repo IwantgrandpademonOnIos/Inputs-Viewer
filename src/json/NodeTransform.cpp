9b171d62c7528a7d83729873f2f033b510fb4dba
When the time comes <293014606+IwantgrandpademonOnIos@users.noreply.github.com>
2026-07-03 17:42:29 +1000

    Update NodeTransform.cpp

--- src/json/NodeTransform.cpp
+++ src/json/NodeTransform.cpp
@@ -1,32 +1,5 @@
-#include "NodeTransform.hpp"
-#include "Cocos.hpp" // IWYU pragma: keep
+#include "IVLevelSettings.hpp"
 
-using namespace geode::prelude;
-
-void NodeTransform::applyTransform(CCNode* node) const {
-    node->setPosition(position);
-    node->setScale(scale);
-    node->setVisible(isVisible);
-}
-
-void NodeTransform::setValues(CCNode* node) {
-    position = node->getPosition();
-    scale = node->getScale();
-    isVisible = node->isVisible();
-}
-
-matjson::Value matjson::Serialize<NodeTransform>::toJson(NodeTransform const& value) {
-    return matjson::makeObject({
-        {"position", value.position},
-        {"scale", value.scale},
-        {"visible", value.isVisible}
-    });
-}
-
-Result<NodeTransform> matjson::Serialize<NodeTransform>::fromJson(matjson::Value const& value) {
-    return Ok(NodeTransform{
-        .position = GEODE_UNWRAP(value["position"].as<CCPoint>()),
-        .scale = GEODE_UNWRAP(value["scale"].as<float>()),
-        .isVisible = GEODE_UNWRAP(value["visible"].asBool())
-    });
-}
+GEODE_NS_IV_BEGIN
+// No implementation needed.
+GEODE_NS_IV_END
