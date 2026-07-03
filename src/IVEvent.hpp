02c4d62c6b12dd0a58e2f66450e81e72687329b1
When the time comes <293014606+IwantgrandpademonOnIos@users.noreply.github.com>
2026-07-03 17:41:01 +1000

    Update IVEvent.hpp

--- src/IVEvent.hpp
+++ src/IVEvent.hpp
@@ -1,41 +1,11 @@
 #pragma once
-
-#include "IVConstants.hpp"
-#include "macros.hpp"
-#include <optional>
 #include <Geode/loader/Event.hpp>
-#include <Geode/utils/function.hpp>
 
 GEODE_NS_IV_BEGIN
 
-enum class SettingEventType {
-    BackgroundColor,
-    OutlineColor,
-    TextColor,
-};
-
-class IVSettingEvent {
-public:
-    IVSettingEvent(SettingEventType type);
-
-    SettingEventType getType() const noexcept;
-
-private:
-    SettingEventType m_type;
-};
-
-class IVSettingFilter {
-public:
-    IVSettingFilter();
-    IVSettingFilter(std::nullopt_t);
-    IVSettingFilter(SettingEventType type);
-
-    using Callback = void(SettingEventType);
-
-    geode::ListenerResult handle(geode::Function<Callback> fn, IVSettingEvent* event);
-
-private:
-    std::optional<SettingEventType> m_type;
+// Minimal event wrapper for future expansion
+struct IVSettingEvent : geode::Event {
+    IVSettingEvent() {}
 };
 
 GEODE_NS_IV_END
