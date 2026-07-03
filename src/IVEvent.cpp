0632f4231cce05cf49518eeb573f9de18161cc35
When the time comes <293014606+IwantgrandpademonOnIos@users.noreply.github.com>
2026-07-03 17:41:40 +1000

    Update IVEvent.cpp

--- src/IVEvent.cpp
+++ src/IVEvent.cpp
@@ -1,31 +1,5 @@
 #include "IVEvent.hpp"
 
-using namespace geode::prelude;
-
 GEODE_NS_IV_BEGIN
-
-IVSettingEvent::IVSettingEvent(SettingEventType type)
-    : m_type(type) {}
-
-SettingEventType IVSettingEvent::getType() const noexcept {
-    return m_type;
-}
-
-IVSettingFilter::IVSettingFilter()
-    : m_type(std::nullopt) {}
-
-IVSettingFilter::IVSettingFilter(std::nullopt_t)
-    : IVSettingFilter() {}
-
-IVSettingFilter::IVSettingFilter(SettingEventType type)
-    : m_type(type) {}
-
-geode::ListenerResult IVSettingFilter::handle(geode::Function<Callback> fn, IVSettingEvent* event) {
-    if (m_type && m_type.value() != event->getType())
-        return geode::ListenerResult::Propagate;
-
-    fn(event->getType());
-    return geode::ListenerResult::Propagate;
-}
-
+// No implementation needed.
 GEODE_NS_IV_END
