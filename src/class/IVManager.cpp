90d377530e1e7c9f0e6b53012b554e36b69e18a8
When the time comes <293014606+IwantgrandpademonOnIos@users.noreply.github.com>
2026-07-03 17:36:14 +1000

    Update IVManager.cpp

--- src/class/IVManager.cpp
+++ src/class/IVManager.cpp
@@ -7,26 +7,11 @@ using namespace geode::prelude;
 // Initializer order doesn't matter here
 #pragma GCC diagnostic ignored "-Wreorder-ctor"
 
-// Your macro stays EXACTLY as your fork defines it
+// This macro now ONLY loads colors.
+// No listeners, no events, no invalid calls.
 #define GEODE_IV_CONSTRUCT_COLOR(__col) \
-    m_##__col##PressColor(Mod::get()->getSettingValue<ccColor4B>(#__col "-press-color")); \
-    m_##__col##ReleaseColor(Mod::get()->getSettingValue<ccColor4B>(#__col "-release-color")); \
-    m_##__col##PressListener( \
-        [this](std::shared_ptr<SettingV3> setting) { \
-            using SettingType = SettingTypeForValueType<ccColor4B>::SettingType; \
-            m_##__col##PressColor = std::static_pointer_cast<SettingType>(setting)->getValue(); \
-            IVSettingEvent(SettingEventType::Color).post(); \
-        } \
-    ); \
-    RGBASettingFilter(Mod::get()->getID(), #__col "-press-color"); \
-    m_##__col##ReleaseListener( \
-        [this](std::shared_ptr<SettingV3> setting) { \
-            using SettingType = SettingTypeForValueType<ccColor4B>::SettingType; \
-            m_##__col##ReleaseColor = std::static_pointer_cast<SettingType>(setting)->getValue(); \
-            IVSettingEvent(SettingEventType::Color).post(); \
-        } \
-    ); \
-    RGBASettingFilter(Mod::get()->getID(), #__col "-release-color");
+    m_##__col##PressColor = Mod::get()->getSettingValue<ccColor4B>(#__col "-press-color"); \
+    m_##__col##ReleaseColor = Mod::get()->getSettingValue<ccColor4B>(#__col "-release-color");
 
 GEODE_NS_IV_BEGIN
 
@@ -43,9 +28,43 @@ IVManager::IVManager()
             .p2Transform = IVManager::getDefaultP2Transform()
         }))
 {
-    // The macro MUST be inside the constructor body.
+    // Load all color settings
     GEODE_IV_CONSTRUCT_COLOR(background)
     GEODE_IV_CONSTRUCT_COLOR(outline)
     GEODE_IV_CONSTRUCT_COLOR(text)
 }
 
+IVManager& IVManager::get() {
+    static IVManager instance;
+    return instance;
+}
+
+NodeTransform IVManager::getDefaultP1Transform() {
+    return {
+        .position = { 0.f, 0.f },
+        .scale = 1.f,
+        .rotation = 0.f,
+        .isVisible = true
+    };
+}
+
+NodeTransform IVManager::getDefaultP2Transform() {
+    return {
+        .position = { 0.f, 0.f },
+        .scale = 1.f,
+        .rotation = 0.f,
+        .isVisible = true
+    };
+}
+
+LevelSettings& IVManager::getLevelSettings(LevelSettingsType type) noexcept {
+    switch (type) {
+        case LevelSettingsType::Classic:
+            return m_settingClassic;
+        case LevelSettingsType::Platformer:
+            return m_settingPlatformer;
+    }
+    return m_settingClassic; // fallback
+}
+
+GEODE_NS_IV_END
