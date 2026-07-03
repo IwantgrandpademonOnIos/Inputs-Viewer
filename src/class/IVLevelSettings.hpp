4b1b5f92ae95d4b0120c9ea0935f1e423ef194e9
When the time comes <293014606+IwantgrandpademonOnIos@users.noreply.github.com>
2026-07-03 17:39:37 +1000

    Update IVLevelSettings.hpp

--- src/class/IVLevelSettings.hpp
+++ src/class/IVLevelSettings.hpp
@@ -1,25 +1,23 @@
 #pragma once
-#include <json/NodeTransform.hpp>
+#include <Geode/Geode.hpp>
 
 GEODE_NS_IV_BEGIN
-
-struct LevelSettings {
-    NodeTransform p1Transform;
-    NodeTransform p2Transform;
-    bool showTotalInputs = false;
-    bool showCPS = false;
-    bool hideLeftRight = false;
-};
 
 enum class LevelSettingsType {
     Classic,
     Platformer
 };
 
-GEODE_NS_IV_END
-
-template<>
-struct matjson::Serialize<inputs_viewer::LevelSettings> {
-    static matjson::Value toJson(inputs_viewer::LevelSettings const& value);
-    static geode::Result<inputs_viewer::LevelSettings> fromJson(matjson::Value const& value);
+struct NodeTransform {
+    cocos2d::CCPoint position;
+    float scale;
+    float rotation;
+    bool isVisible;
 };
+
+struct LevelSettings {
+    NodeTransform p1Transform;
+    NodeTransform p2Transform;
+};
+
+GEODE_NS_IV_END
