18ac327afdbe35714b2e8350bbaaf144ccb239d6
When the time comes <293014606+IwantgrandpademonOnIos@users.noreply.github.com>
2026-07-03 17:56:28 +1000

    Update IVBackgroundSprite.cpp

--- src/class/IVBackgroundSprite.cpp
+++ src/class/IVBackgroundSprite.cpp
@@ -5,14 +5,15 @@ using namespace geode::prelude;
 
 GEODE_NS_IV_BEGIN
 
-BackgroundSprite::BackgroundSprite(ccColor4B const& backgroundColor, ccColor4B const& outlineColor, ccColor4B const& textColor)
+BackgroundSprite::BackgroundSprite(
+    cocos2d::ccColor4B const& backgroundColor,
+    cocos2d::ccColor4B const& outlineColor,
+    cocos2d::ccColor4B const& textColor
+)
     : m_backgroundColor(backgroundColor)
     , m_outlineColor(outlineColor)
     , m_textColor(textColor)
-{
-    // Geode 5 removed EventListener, so the old listener is gone.
-    // Your sprite still updates correctly when you call the setters.
-}
+{}
 
 BackgroundSprite::BackgroundSprite()
     : BackgroundSprite(
@@ -21,6 +22,20 @@ BackgroundSprite::BackgroundSprite()
         IVManager::get().m_textReleaseColor
     )
 {}
+
+BackgroundSprite* BackgroundSprite::create(
+    cocos2d::ccColor4B const& backgroundColor,
+    cocos2d::ccColor4B const& outlineColor,
+    cocos2d::ccColor4B const& textColor
+) {
+    auto ret = new (std::nothrow) BackgroundSprite(backgroundColor, outlineColor, textColor);
+    if (ret && ret->init()) {
+        ret->autorelease();
+        return ret;
+    }
+    delete ret;
+    return nullptr;
+}
 
 BackgroundSprite* BackgroundSprite::create() {
     auto ret = new (std::nothrow) BackgroundSprite;
@@ -38,16 +53,124 @@ bool BackgroundSprite::init() {
 
     m_fill = CCScale9Sprite::create("background_fill.png"_spr);
     this->addChild(m_fill);
-    this->addBackgroundNode(m_fill);
+    addBackgroundNode(m_fill);
 
     m_outline = CCScale9Sprite::create("background_outline.png"_spr);
     this->addChild(m_outline);
-    this->addOutlineNode(m_outline);
+    addOutlineNode(m_outline);
+
+    updateBackgroundColorNodes();
+    updateOutlineColorNodes();
+    updateTextColorNodes();
+
     return true;
 }
 
-    GEODE_NS_IV_END
+void BackgroundSprite::addBackgroundNode(cocos2d::CCNode* node) {
+    if (!node) return;
+    m_backgroundColorNodes.addObject(node);
+    m_shouldUpdateLayout = true;
+}
 
+void BackgroundSprite::addOutlineNode(cocos2d::CCNode* node) {
+    if (!node) return;
+    m_outlineColorNodes.addObject(node);
+    m_shouldUpdateLayout = true;
+}
 
+void BackgroundSprite::addTextNode(cocos2d::CCNode* node) {
+    if (!node) return;
+    m_textColorNodes.addObject(node);
+    m_shouldUpdateLayout = true;
+}
 
+void BackgroundSprite::removeBackgroundNode(cocos2d::CCNode* node) {
+    if (!node) return;
+    m_backgroundColorNodes.removeObject(node);
+    m_shouldUpdateLayout = true;
+}
 
+void BackgroundSprite::removeOutlineNode(cocos2d::CCNode* node) {
+    if (!node) return;
+    m_outlineColorNodes.removeObject(node);
+    m_shouldUpdateLayout = true;
+}
+
+void BackgroundSprite::removeTextNode(cocos2d::CCNode* node) {
+    if (!node) return;
+    m_textColorNodes.removeObject(node);
+    m_shouldUpdateLayout = true;
+}
+
+void BackgroundSprite::setBackgroundColor(cocos2d::ccColor4B const& color) {
+    m_backgroundColor = color;
+    updateBackgroundColorNodes();
+}
+
+void BackgroundSprite::setOutlineColor(cocos2d::ccColor4B const& color) {
+    m_outlineColor = color;
+    updateOutlineColorNodes();
+}
+
+void BackgroundSprite::setTextColor(cocos2d::ccColor4B const& color) {
+    m_textColor = color;
+    updateTextColorNodes();
+}
+
+void BackgroundSprite::setContentSize(cocos2d::CCSize const& size) {
+    CCNode::setContentSize(size);
+    if (m_fill) {
+        m_fill->setContentSize(size);
+    }
+    if (m_outline) {
+        m_outline->setContentSize(size);
+    }
+    m_shouldUpdateLayout = true;
+}
+
+void BackgroundSprite::visit() {
+    if (m_shouldUpdateLayout) {
+        updateBackgroundColorNodes();
+        updateOutlineColorNodes();
+        updateTextColorNodes();
+        m_shouldUpdateLayout = false;
+    }
+    CCNode::visit();
+}
+
+void BackgroundSprite::updateBackgroundColorNodes() {
+    for (auto node : m_backgroundColorNodes) {
+        if (auto sprite = typeinfo_cast<CCSprite*>(node)) {
+            sprite->setColor({ m_backgroundColor.get().r, m_backgroundColor.get().g, m_backgroundColor.get().b });
+            sprite->setOpacity(m_backgroundColor.get().a);
+        }
+    }
+    if (m_fill) {
+        m_fill->setColor({ m_backgroundColor.get().r, m_backgroundColor.get().g, m_backgroundColor.get().b });
+        m_fill->setOpacity(m_backgroundColor.get().a);
+    }
+}
+
+void BackgroundSprite::updateOutlineColorNodes() {
+    for (auto node : m_outlineColorNodes) {
+        if (auto sprite = typeinfo_cast<CCSprite*>(node)) {
+            sprite->setColor({ m_outlineColor.get().r, m_outlineColor.get().g, m_outlineColor.get().b });
+            sprite->setOpacity(m_outlineColor.get().a);
+        }
+    }
+    if (m_outline) {
+        m_outline->setColor({ m_outlineColor.get().r, m_outlineColor.get().g, m_outlineColor.get().b });
+        m_outline->setOpacity(m_outlineColor.get().a);
+    }
+}
+
+void BackgroundSprite::updateTextColorNodes() {
+    for (auto node : m_textColorNodes) {
+        if (auto label = typeinfo_cast<CCLabelBMFont*>(node)) {
+            label->setColor({ m_textColor.get().r, m_textColor.get().g, m_textColor.get().b });
+            label->setOpacity(m_textColor.get().a);
+        }
+    }
+}
+
+GEODE_NS_IV_END
