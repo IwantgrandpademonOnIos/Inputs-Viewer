24cae03b200b5acaf103eaa4f8810932e0cc3ad7
When the time comes <293014606+IwantgrandpademonOnIos@users.noreply.github.com>
2026-07-04 09:56:43 +1000

    Update IVBackgroundSprite.cpp

--- src/class/IVBackgroundSprite.cpp
+++ src/class/IVBackgroundSprite.cpp
@@ -5,11 +5,9 @@ using namespace geode::prelude;
 
 GEODE_NS_IV_BEGIN
 
-BackgroundSprite::BackgroundSprite(
-    cocos2d::ccColor4B const& backgroundColor,
-    cocos2d::ccColor4B const& outlineColor,
-    cocos2d::ccColor4B const& textColor
-)
+BackgroundSprite::BackgroundSprite(ccColor4B const& backgroundColor,
+                                   ccColor4B const& outlineColor,
+                                   ccColor4B const& textColor)
     : m_backgroundColor(backgroundColor)
     , m_outlineColor(outlineColor)
     , m_textColor(textColor)
@@ -23,20 +21,6 @@ BackgroundSprite::BackgroundSprite()
     )
 {}
 
-BackgroundSprite* BackgroundSprite::create(
-    cocos2d::ccColor4B const& backgroundColor,
-    cocos2d::ccColor4B const& outlineColor,
-    cocos2d::ccColor4B const& textColor
-) {
-    auto ret = new (std::nothrow) BackgroundSprite(backgroundColor, outlineColor, textColor);
-    if (ret && ret->init()) {
-        ret->autorelease();
-        return ret;
-    }
-    delete ret;
-    return nullptr;
-}
-
 BackgroundSprite* BackgroundSprite::create() {
     auto ret = new (std::nothrow) BackgroundSprite;
     if (ret && ret->init()) {
@@ -49,126 +33,101 @@ BackgroundSprite* BackgroundSprite::create() {
 
 bool BackgroundSprite::init() {
     if (!CCNode::init()) return false;
-    this->setAnchorPoint(ccp(0.5f, 0.5f));
+    this->setAnchorPoint({0.5f, 0.5f});
 
     m_fill = CCScale9Sprite::create("background_fill.png"_spr);
     this->addChild(m_fill);
-    addBackgroundNode(m_fill);
+    this->addBackgroundNode(m_fill);
 
     m_outline = CCScale9Sprite::create("background_outline.png"_spr);
     this->addChild(m_outline);
-    addOutlineNode(m_outline);
+    this->addOutlineNode(m_outline);
 
-    updateBackgroundColorNodes();
-    updateOutlineColorNodes();
-    updateTextColorNodes();
-
+    this->setContentSize(m_outline->getContentSize());
     return true;
 }
 
-void BackgroundSprite::addBackgroundNode(cocos2d::CCNode* node) {
-    if (!node) return;
-    m_backgroundColorNodes.push_back(node);
-    m_shouldUpdateLayout = true;
-}
-
-void BackgroundSprite::addOutlineNode(cocos2d::CCNode* node) {
-    if (!node) return;
-    m_outlineColorNodes.push_back(node);
-    m_shouldUpdateLayout = true;
-}
-
-void BackgroundSprite::addTextNode(cocos2d::CCNode* node) {
-    if (!node) return;
-    m_textColorNodes.push_back(node);
-    m_shouldUpdateLayout = true;
-}
-
-void BackgroundSprite::removeBackgroundNode(cocos2d::CCNode* node) {
-    if (!node) return;
-    m_backgroundColorNodes.remove(node);
-    m_shouldUpdateLayout = true;
-}
-
-void BackgroundSprite::removeOutlineNode(cocos2d::CCNode* node) {
-    if (!node) return;
-    m_outlineColorNodes.remove(node);
-    m_shouldUpdateLayout = true;
-}
-
-void BackgroundSprite::removeTextNode(cocos2d::CCNode* node) {
-    if (!node) return;
-    m_textColorNodes.remove(node);
-    m_shouldUpdateLayout = true;
-}
-
-void BackgroundSprite::setBackgroundColor(cocos2d::ccColor4B const& color) {
-    m_backgroundColor = color;
-    updateBackgroundColorNodes();
-}
-
-void BackgroundSprite::setOutlineColor(cocos2d::ccColor4B const& color) {
-    m_outlineColor = color;
-    updateOutlineColorNodes();
-}
-
-void BackgroundSprite::setTextColor(cocos2d::ccColor4B const& color) {
-    m_textColor = color;
-    updateTextColorNodes();
-}
-
-void BackgroundSprite::setContentSize(cocos2d::CCSize const& size) {
+void BackgroundSprite::setContentSize(CCSize const& size) {
     CCNode::setContentSize(size);
-    if (m_fill) {
-        m_fill->setContentSize(size);
-    }
-    if (m_outline) {
-        m_outline->setContentSize(size);
-    }
-    m_shouldUpdateLayout = true;
 }
 
-void BackgroundSprite::visit() {
-    if (m_shouldUpdateLayout) {
-        updateBackgroundColorNodes();
-        updateOutlineColorNodes();
-        updateTextColorNodes();
-        m_shouldUpdateLayout = false;
+void BackgroundSprite::setBackgroundColor(ccColor4B const& color) {
+    m_backgroundColor = color;
+    this->updateBackgroundColorNodes();
+}
+
+void BackgroundSprite::setOutlineColor(ccColor4B const& color) {
+    m_outlineColor = color;
+    this->updateOutlineColorNodes();
+}
+
+void BackgroundSprite::setTextColor(ccColor4B const& color) {
+    m_textColor = color;
+    this->updateTextColorNodes();
+}
+
+void BackgroundSprite::addBackgroundNode(CCNode* node) {
+    if (auto protocol = typeinfo_cast<CCRGBAProtocol*>(node)) {
+        protocol->setColor(m_backgroundColor);
+        m_backgroundColorNodes.push_back(node);
     }
-    CCNode::visit();
+}
+
+void BackgroundSprite::addOutlineNode(CCNode* node) {
+    if (auto protocol = typeinfo_cast<CCRGBAProtocol*>(node)) {
+        protocol->setColor(m_outlineColor);
+        m_outlineColorNodes.push_back(node);
+    }
+}
+
+void BackgroundSprite::addTextNode(CCNode* node) {
+    if (auto protocol = typeinfo_cast<CCRGBAProtocol*>(node)) {
+        protocol->setColor(m_textColor);
+        m_textColorNodes.push_back(node);
+    }
+}
+
+void BackgroundSprite::removeBackgroundNode(CCNode* node) {
+    m_backgroundColorNodes.erase(
+        std::remove(m_backgroundColorNodes.begin(), m_backgroundColorNodes.end(), node),
+        m_backgroundColorNodes.end()
+    );
+}
+
+void BackgroundSprite::removeOutlineNode(CCNode* node) {
+    m_outlineColorNodes.erase(
+        std::remove(m_outlineColorNodes.begin(), m_outlineColorNodes.end(), node),
+        m_outlineColorNodes.end()
+    );
+}
+
+void BackgroundSprite::removeTextNode(CCNode* node) {
+    m_textColorNodes.erase(
+        std::remove(m_textColorNodes.begin(), m_textColorNodes.end(), node),
+        m_textColorNodes.end()
+    );
 }
 
 void BackgroundSprite::updateBackgroundColorNodes() {
     for (auto node : m_backgroundColorNodes) {
-        if (auto sprite = typeinfo_cast<CCSprite*>(node)) {
-            sprite->setColor({ m_backgroundColor.get().r, m_backgroundColor.get().g, m_backgroundColor.get().b });
-            sprite->setOpacity(m_backgroundColor.get().a);
+        if (auto protocol = typeinfo_cast<CCRGBAProtocol*>(node)) {
+            protocol->setColor(m_backgroundColor);
         }
-    }
-    if (m_fill) {
-        m_fill->setColor({ m_backgroundColor.get().r, m_backgroundColor.get().g, m_backgroundColor.get().b });
-        m_fill->setOpacity(m_backgroundColor.get().a);
     }
 }
 
 void BackgroundSprite::updateOutlineColorNodes() {
     for (auto node : m_outlineColorNodes) {
-        if (auto sprite = typeinfo_cast<CCSprite*>(node)) {
-            sprite->setColor({ m_outlineColor.get().r, m_outlineColor.get().g, m_outlineColor.get().b });
-            sprite->setOpacity(m_outlineColor.get().a);
+        if (auto protocol = typeinfo_cast<CCRGBAProtocol*>(node)) {
+            protocol->setColor(m_outlineColor);
         }
-    }
-    if (m_outline) {
-        m_outline->setColor({ m_outlineColor.get().r, m_outlineColor.get().g, m_outlineColor.get().b });
-        m_outline->setOpacity(m_outlineColor.get().a);
     }
 }
 
 void BackgroundSprite::updateTextColorNodes() {
     for (auto node : m_textColorNodes) {
-        if (auto label = typeinfo_cast<CCLabelBMFont*>(node)) {
-            label->setColor({ m_textColor.get().r, m_textColor.get().g, m_textColor.get().b });
-            label->setOpacity(m_textColor.get().a);
+        if (auto protocol = typeinfo_cast<CCRGBAProtocol*>(node)) {
+            protocol->setColor(m_textColor);
         }
     }
 }
