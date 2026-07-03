36b247cc014189be04eb5f65a25faea703ffdd1c
When the time comes <293014606+IwantgrandpademonOnIos@users.noreply.github.com>
2026-07-04 09:57:28 +1000

    Update IVBackgroundSprite.hpp

--- src/class/IVBackgroundSprite.hpp
+++ src/class/IVBackgroundSprite.hpp
@@ -1,25 +1,30 @@
 #pragma once
-#include "IVEvent.hpp"
+
+#include <Geode/Geode.hpp>
+#include <Geode/modify/CCNode.hpp>
+#include <Geode/utils/cocos.hpp>
+
+#include <vector>
 
 GEODE_NS_IV_BEGIN
 
 class BackgroundSprite : public cocos2d::CCNode {
 public:
-    BackgroundSprite(
-        cocos2d::ccColor4B const& backgroundColor,
-        cocos2d::ccColor4B const& outlineColor,
-        cocos2d::ccColor4B const& textColor
-    );
     BackgroundSprite();
-    static BackgroundSprite* create(
-        cocos2d::ccColor4B const& backgroundColor,
-        cocos2d::ccColor4B const& outlineColor,
-        cocos2d::ccColor4B const& textColor
-    );
+    BackgroundSprite(ccColor4B const& backgroundColor,
+                     ccColor4B const& outlineColor,
+                     ccColor4B const& textColor);
+
     static BackgroundSprite* create();
+
     bool init() override;
 
-public:
+    void setContentSize(CCSize const& size) override;
+
+    void setBackgroundColor(ccColor4B const& color);
+    void setOutlineColor(ccColor4B const& color);
+    void setTextColor(ccColor4B const& color);
+
     void addBackgroundNode(cocos2d::CCNode* node);
     void addOutlineNode(cocos2d::CCNode* node);
     void addTextNode(cocos2d::CCNode* node);
@@ -28,35 +33,23 @@ public:
     void removeOutlineNode(cocos2d::CCNode* node);
     void removeTextNode(cocos2d::CCNode* node);
 
-    void setBackgroundColor(cocos2d::ccColor4B const& color);
-    void setOutlineColor(cocos2d::ccColor4B const& color);
-    void setTextColor(cocos2d::ccColor4B const& color);
-
-public:
-    void setContentSize(cocos2d::CCSize const& size) override;
-    void visit() override;
-
-protected:
+private:
     void updateBackgroundColorNodes();
     void updateOutlineColorNodes();
     void updateTextColorNodes();
 
-protected:
-    cocos2d::extension::CCScale9Sprite* m_fill = nullptr;
-    cocos2d::extension::CCScale9Sprite* m_outline = nullptr;
+    cocos2d::CCScale9Sprite* m_fill = nullptr;
+    cocos2d::CCScale9Sprite* m_outline = nullptr;
 
-    bool m_shouldUpdateLayout = false;
+    ccColor4B m_backgroundColor;
+    ccColor4B m_outlineColor;
+    ccColor4B m_textColor;
 
-    std::reference_wrapper<cocos2d::ccColor4B const> m_backgroundColor;
-    std::reference_wrapper<cocos2d::ccColor4B const> m_outlineColor;
-    std::reference_wrapper<cocos2d::ccColor4B const> m_textColor;
-
-    geode::cocos::CCArrayExt<cocos2d::CCNode*> m_backgroundColorNodes;
-    geode::cocos::CCArrayExt<cocos2d::CCNode*> m_outlineColorNodes;
-    geode::cocos::CCArrayExt<cocos2d::CCNode*> m_textColorNodes;
-
-    // Geode 5 removed EventListener entirely, so this member is gone.
-    // geode::EventListener<IVSettingFilter> m_colorListener;
+    // Replaced CCArrayExt with std::vector
+    std::vector<cocos2d::CCNode*> m_backgroundColorNodes;
+    std::vector<cocos2d::CCNode*> m_outlineColorNodes;
+    std::vector<cocos2d::CCNode*> m_textColorNodes;
+    std::vector<cocos2d::CCNode*> m_tColorNodes;
 };
 
 GEODE_NS_IV_END
