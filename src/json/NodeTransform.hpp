28fec8ab01813fb51c77df790cfee181088f5963
When the time comes <293014606+IwantgrandpademonOnIos@users.noreply.github.com>
2026-07-03 18:12:20 +1000

    Update NodeTransform.hpp

--- src/json/NodeTransform.hpp
+++ src/json/NodeTransform.hpp
@@ -1,18 +1,16 @@
 #pragma once
+#include <Geode/Geode.hpp>
+
+GEODE_NS_IV_BEGIN
 
 struct NodeTransform {
-    cocos2d::CCPoint position{};
+    cocos2d::CCPoint position{0.f, 0.f};
     float scale{1.f};
-    bool isVisible = true;
+    float rotation{0.f};
+    bool isVisible{true};
 
-    // Apply transform to a node
     void applyTransform(cocos2d::CCNode* node) const;
-    // Set transform from a node
     void setValues(cocos2d::CCNode* node);
 };
 
-template<>
-struct matjson::Serialize<NodeTransform> {
-    static matjson::Value toJson(NodeTransform const& value);
-    static geode::Result<NodeTransform> fromJson(matjson::Value const& value);
-};
+GEODE_NS_IV_END
